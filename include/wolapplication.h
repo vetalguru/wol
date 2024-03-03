9a9267b7ccb97bb174d64dd6e2aaa0fe61dbae1c
Vitalii Shkibtan <vetalguru@gmail.com>
2024-03-03 23:29:57 +0200

    Add repo to the github

---
+++ include/wolapplication.h
@@ -0,0 +1,44 @@
+#ifndef WOLAPPLICATION_H
+#define WOLAPPLICATION_H
+
+#include <array>
+#include <string>
+#include <vector>
+
+class WolApplication
+{
+    public:
+        WolApplication(const int argc, const char* const argv[]);
+        WolApplication(const WolApplication&) = delete;
+        WolApplication(WolApplication&&) = delete;
+        ~WolApplication();
+
+        int run() noexcept;
+
+        WolApplication& operator = (const WolApplication&) = delete;
+        WolApplication& operator = (WolApplication&&) = delete;
+
+    private:
+        static constexpr size_t MAC_STR_LENGTH{17};
+        static constexpr size_t MAC_BYTES_LENGTH{6};
+        static constexpr size_t MAC_MAGIC_PACKAGE_LENGTH{102};
+        static constexpr int    DEFAULT_WOL_PORT{9};
+
+    private:
+        void parseParams(const int argc, const char* const argv[]) noexcept;
+        bool validateParams(const std::vector<std::string>& params) noexcept;
+        bool validateMacAddrString(const std::string& macStr) noexcept;
+        bool converMacStrToBytes(const std::string& macStr, std::array<char, MAC_BYTES_LENGTH>& mac) noexcept;
+        void generateMagicPackage(const std::array<char, MAC_BYTES_LENGTH>& mac,
+                                  std::array<char, MAC_MAGIC_PACKAGE_LENGTH>& magicPackage) noexcept;
+        bool sendMagicPackage(const std::array<char, MAC_BYTES_LENGTH>& mac,
+                              const std::array<char, MAC_MAGIC_PACKAGE_LENGTH>& magicPackage,
+                              const int port = DEFAULT_WOL_PORT) noexcept;
+
+        void printHelpMessage() const;
+
+    private:
+        std::vector<std::string> m_params;
+};
+
+#endif // WOLAPPLICATION_H
